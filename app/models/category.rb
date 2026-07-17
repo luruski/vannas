2727d18cd37b00ef3ac3efd35685ed80744e7212
luruski <luruski@rrc.ca>
2026-07-16 23:08:07 -0500

    Fix Ransack allowlist for all models

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["products"]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,12 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :available, inclusion: { in: [true, false] }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["available", "category_id", "created_at", "description", "id", "image", "name", "price", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["category"]
+  end
 end
