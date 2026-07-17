8852a158d9901846c2b2b81b8b2c5f4e990606db
luruski <luruski@rrc.ca>
2026-07-16 22:57:36 -0500

    Add validations and associations to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :available, inclusion: { in: [true, false] }
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,9 @@
 class Province < ApplicationRecord
+  has_many :users
+
+  validates :name, presence: true, uniqueness: true
+  validates :abbreviation, presence: true, uniqueness: true
+  validates :gst_rate, numericality: { greater_than_or_equal_to: 0 }
+  validates :pst_rate, numericality: { greater_than_or_equal_to: 0 }
+  validates :hst_rate, numericality: { greater_than_or_equal_to: 0 }
 end
