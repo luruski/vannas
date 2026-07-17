b039355df28284c43b461ab195675d25daa5b3f9
luruski <luruski@rrc.ca>
2026-07-17 00:36:09 -0500

    Add keyword search with category dropdown filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,17 @@ class ProductsController < ApplicationController
   def index
     @categories = Category.all
     @products = Product.where(available: true).includes(:category)
+
     if params[:category_id].present?
       @products = @products.where(category_id: params[:category_id])
+    end
+
+    if params[:search].present?
+      @products = @products.where(
+        "name LIKE ? OR description LIKE ?",
+        "%#{params[:search]}%",
+        "%#{params[:search]}%"
+      )
     end
   end
 
