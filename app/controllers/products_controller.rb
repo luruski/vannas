297a19acceffd700e7ca204612ce32fb5f6ba05f
luruski <luruski@rrc.ca>
2026-07-16 23:42:41 -0500

    Add category navigation filter to product index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    @categories = Category.all
     @products = Product.where(available: true).includes(:category)
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
   end
 
   def show
